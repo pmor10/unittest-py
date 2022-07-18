21a8f492212f2c729998a838e0b220ca89df03e4
pmor10 <paresamorton@gmail.com>
2022-07-17 21:59:19 -0700

    unittest

--- testing/main.py
+++ testing/main.py
@@ -1,1 +1,12 @@
-def do_stuff(num):
+from tempfile import TemporaryFile
+from typing import Type
+
+
+def do_stuff(num=0):
+    try:
+        if num:
+            return int(num) + 5
+        else:
+            return 'please enter a number'
+    except ValueError as err:
+        return err
